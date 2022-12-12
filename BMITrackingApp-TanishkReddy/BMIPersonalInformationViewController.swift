80d96d527de524909d083f01c9740d1088870ae4
TanishkReddy07 <tanishkreddy512@gmail.com>
2022-12-12 13:36:15 -0500

    Inital project commited

---
+++ BMITrackingApp-TanishkReddy/BMIPersonalInformationViewController.swift
@@ -0,0 +1,19 @@
+//
+//  BMIPersonalInformationViewController.swift
+//  BMITrackingApp-TanishkReddy
+//
+//  Created by Tanishk Sai Reddy Peruvala-301293616 on 2022-12-12.
+//
+
+import UIKit
+
+class BMIPersonalInformationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
